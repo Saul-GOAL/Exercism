f40d105fb254d2ad1a030c8b356eb9867ed02118
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-03-15 11:59:17 +0100

    Add files via upload

---
+++ Booleans/AnnalynsInfiltration/AnnalynsInfiltration/Program.cs
@@ -0,0 +1,19 @@
+﻿static class QuestLogic
+{
+    public static void Main(string[] args)
+    {
+
+    }
+
+    public static bool CanFastAttack(bool knightIsAwake) => 
+        !knightIsAwake;
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake) => 
+        (knightIsAwake || archerIsAwake || prisonerIsAwake) ? true : false;
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake) =>
+        archerIsAwake == false && prisonerIsAwake;
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent) => 
+        petDogIsPresent ? archerIsAwake == false : (prisonerIsAwake) && ((archerIsAwake == false) && (knightIsAwake == false));
+}
