106b468cb837f6b58f45ff08a2c4f549a027d12c
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-03-15 13:10:21 +0100

    Add files via upload

---
+++ Strings/Bob/Bob/Program.cs
@@ -0,0 +1,34 @@
+﻿public static class Bob
+{
+    //public static void Main(string[] args)
+    //{
+    //    var statement = "I HATE THE DENTIST";
+
+    //    Console.WriteLine($"{Response(statement)}");
+    //    Console.ReadLine();
+    //}
+
+    private static string _defaultResponse = "Whatever.";
+    private static string _responseToYelling = "Whoa, chill out!";
+    private static string _responseToQuestion = "Sure.";
+    private static string _responseToYellingQuestion = "Calm down, I know what I'm doing!";
+    private static string _responseToSilence = "Fine. Be that way!";
+
+    public static string Response(string statement)
+    {
+        var statement_IsQuestion = statement.Trim().EndsWith("?");
+        var statement_IsYell = statement.Any(ch => char.IsLetter(ch) && statement.ToUpperInvariant() == statement);
+        var statement_IsSilent = string.IsNullOrEmpty(statement);
+
+        if (statement_IsSilent)
+            return _responseToSilence;
+        if (statement_IsQuestion && statement_IsYell)
+            return _responseToYellingQuestion;
+        if (statement_IsQuestion)
+            return _responseToQuestion;
+        if (statement_IsYell)
+            return _responseToYelling;
+        return _defaultResponse;
+    }
+
+}
