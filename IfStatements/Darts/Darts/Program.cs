1ac313a4b17f472f0be1a79e49899988c8c68f16
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-03-22 18:07:28 +0100

    Add files via upload

---
+++ IfStatements/Darts/Darts/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Darts
+{
+    public static void Main(string[] args)
+    {
+        var dartPosX = 3;
+        var dartPosY = 4;
+
+        var points = Score(dartPosX, dartPosY);
+        Console.WriteLine($"Your dart goes to the ({dartPosX},{dartPosY}) so it gets {points}");
+        Console.ReadLine();
+    }
+
+    public static int Score(double x, double y)
+    {
+        double distance = Math.Sqrt(x * x + y * y);
+        return distance switch
+        {
+            <= 1 => 10,
+            <= 5 => 5,
+            <= 10 => 1,
+            _ => 0,
+        };
+    }
+}
