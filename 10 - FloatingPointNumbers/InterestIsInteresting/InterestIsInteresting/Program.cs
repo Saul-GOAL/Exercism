21ce0d83c70c6e5fae1878c6ee4581fe7d27aea4
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-16 15:06:13 +0200

    Add files via upload

---
+++ 10 - FloatingPointNumbers/InterestIsInteresting/InterestIsInteresting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+static class SavingsAccount
+{
+    public static float InterestRate(decimal balance) =>
+        balance switch
+        {
+            < 0 => 3.213f,
+            < 1_000 => 0.5f,
+            >= 1_000 and < 5_000 => 1.621f,
+            _ => 2.475f
+        };
+
+    public static decimal Interest(decimal balance) => 
+        balance * ((decimal)InterestRate(balance) / 100);
+
+    public static decimal AnnualBalanceUpdate(decimal balance) =>
+        balance + Interest(balance);
+
+    public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+    {
+        var years = 0;
+        while (targetBalance > balance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            years++;
+        }
+        return years;
+    }
+}
