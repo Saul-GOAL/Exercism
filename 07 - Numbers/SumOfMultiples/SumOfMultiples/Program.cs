f33494e15c84f259d294cab56ea57d56935da7c8
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-16 14:56:44 +0200

    Add files via upload

---
+++ 07 - Numbers/SumOfMultiples/SumOfMultiples/Program.cs
@@ -0,0 +1,28 @@
+﻿public static class SumOfMultiples
+{
+    public static void Main()
+    {
+        var multiples = new[] { 3, 5};
+
+        var result = Sum(multiples, 10);
+
+        Console.WriteLine(result);
+
+        Console.ReadLine();
+    }
+
+
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        var listaElementos = new List<int>();
+
+        foreach (var number in multiples.Where(x => x != 0))
+        {
+            for (int i = number; i < max; i += number)
+            {
+                listaElementos.Add(i);
+            }
+        }
+        return listaElementos.Distinct().Sum();
+    }
+}
