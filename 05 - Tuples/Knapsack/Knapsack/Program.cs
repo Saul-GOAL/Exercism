81406e6e4613455deb784909754ecbe74c9463fa
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-16 18:02:09 +0200

    Add files via upload

---
+++ 05 - Tuples/Knapsack/Knapsack/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public static class Knapsack
+{
+    public static int MaximumValue(int maxWeight, (int weight, int value)[] items)
+    {
+        var dp = new int[maxWeight + 1];
+
+        foreach (var (weight, value) in items)
+            for (var i = maxWeight; i >= weight; i--)
+                dp[i] = Math.Max(dp[i], dp[i - weight] + value);
+        return dp[maxWeight];
+    }
+}
