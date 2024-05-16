247675b3a655b35967e3a103705d6618f2b28324
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-16 14:14:08 +0200

    Add files via upload

---
+++ 08 - Nullability/TimFromMarketing/TimFromMarketing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+static class Badge
+{
+    public static void Main()
+    {
+        var result = Print(59, "Julie Sokato", null);
+
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+
+
+    public static string Print(int? id, string name, string? department)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        if (id != null)
+            sb.Append("[" + id + "] - ");
+
+        sb.Append(name);
+
+        if (department == null)
+            sb.Append(" - OWNER");
+        else
+            sb.Append(" - " + department.ToUpper());
+
+        return sb.ToString();
+    }
+}
+
