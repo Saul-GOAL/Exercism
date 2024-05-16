de2268a0c310919306c7db132fa4343d9ae50cd6
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-16 14:32:44 +0200

    Add files via upload

---
+++ 05 - Tuples/PhoneCallAnalysis/PhoneCallAnalysis/Program.cs
@@ -0,0 +1,22 @@
+﻿
+public class PhoneCallAnalysis
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a phone number like 'NNN-NNN-NNNN': ");
+        var phoneNumber = Console.ReadLine();
+
+        (bool IsNewYork, bool isFake, string localNumber) = Analyze(phoneNumber);
+        Console.WriteLine($"\n The analysis form the number {phoneNumber} is: \n It is from New York?: {IsNewYork} \n Is Fake?: {isFake} \n Local NUmber: {localNumber}");
+        Console.ReadLine();
+    }
+
+    public static (bool IsNewYork, bool isFake, string localNumber) Analyze(string phoneNumber)
+    {
+        var numbers = phoneNumber.Split("-");
+        return (numbers[0].Equals("212"), numbers[1].Equals("555"), numbers[2]);
+    }
+
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) =>
+        phoneNumberInfo.IsFake;
+}
