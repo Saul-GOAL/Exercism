ea53bbe2678388400ab915a46a15d99a81cc9f8a
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-17 12:40:09 +0200

    Add files via upload

---
+++ 07 - Numbers/SpiralMatrix/SpiralMatrix/Program.cs
@@ -0,0 +1,30 @@
+﻿public class SpiralMatrix
+{
+    public static int[,] GetMatrix(int size)
+    {
+        int[,] matrix = new int[size, size];
+
+        int col = 0, row = 0, max = size - 1, min = 0;
+
+        for (int i = 1; i <= size * size; i++)
+        {
+            matrix[row, col] = i;
+
+            if (row == max && col != min)
+                col--;
+            else if (col == max)
+                row++;
+            else if (row == min)
+                col++;
+            else if (col == min && row != min + 1)
+                row--;
+            else
+            {
+                max -= 1;
+                min += 1;
+                col++;
+            }
+        }
+        return matrix;
+    }
+}
