7692bca5cdd84f9f6cccddbf8a979aeef8934f29
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-17 18:45:59 +0200

    Add files via upload

---
+++ 16 - Arrays/ResistorColorDuo/ResistorColorDuo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class ResistorColorDuo
+{
+    private static readonly string[] resistorColors = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+    public static int Value(string[] colors) => colors.Length switch
+    {
+        < 2 => Array.IndexOf(resistorColors, colors[0]),
+        >= 2 => Array.IndexOf(resistorColors, colors[0]) * 10 + Array.IndexOf(resistorColors, colors[1]),
+        //_ => 0
+    };
+}
