2f0afa80af0e8eb59256895a440b08612be4050d
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-17 19:07:40 +0200

    Add files via upload

---
+++ 19 - Inheritance/WizardsAndWarriors/WizardsAndWarriors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+abstract class Character
+{
+    protected string CharacterType => 
+        GetType().Name;
+
+    public abstract int DamagePoints(Character target);
+
+    public virtual bool Vulnerable() => 
+        false;
+
+    public override string ToString() => 
+        $"Character is a {CharacterType}";
+}
+
+class Warrior : Character
+{
+    public override int DamagePoints(Character target) => 
+        (target.Vulnerable() ? 10 : 6);
+
+}
+
+class Wizard : Character
+{
+    private bool isSpellPrepared = false;
+
+    public override int DamagePoints(Character target) => 
+        (isSpellPrepared ? 12 : 3);
+
+    public override bool Vulnerable() => 
+        !(isSpellPrepared);
+
+    public void PrepareSpell() => 
+        isSpellPrepared = true;
+}
