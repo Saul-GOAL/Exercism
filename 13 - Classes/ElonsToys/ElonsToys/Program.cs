4dd6d8dfd75d323f9301770033da72c2560f07c2
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-17 17:59:20 +0200

    Add files via upload

---
+++ 13 - Classes/ElonsToys/ElonsToys/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class RemoteControlCar
+{
+
+    private int _batteryLevel = 100;
+    private int _distanceDriven = 0;
+
+    public static RemoteControlCar Buy() => new();
+
+    public string DistanceDisplay() =>
+        $"Driven {_distanceDriven} meters";
+
+    public string BatteryDisplay() => 
+        _batteryLevel > 0 ? $"Battery at {_batteryLevel}%" : "Battery empty";
+
+    public void Drive()
+    {
+        if (_batteryLevel > 0)
+        {
+            _distanceDriven += 20;
+            _batteryLevel--;
+        }
+    }
+}
