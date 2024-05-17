a0c1509f0ae7b17d279b815ec5688f09a2b52de9
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-17 17:42:25 +0200

    Add files via upload

---
+++ 27 - Enums/Allergies/Allergies/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Allergen
+{
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128
+}
+
+public class Allergies
+{
+ 
+    private Allergen allergens;
+
+    public Allergies(int mask) => 
+        allergens = (Allergen)mask;
+
+    public bool IsAllergicTo(Allergen allergen) =>
+        allergens.HasFlag(allergen);
+
+    public Allergen[] List() =>
+        Enum.GetValues<Allergen>().Where(a => allergens.HasFlag(a)).ToArray();
+}
