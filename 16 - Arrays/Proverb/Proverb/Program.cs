f1188719f03b27594dcd5405588ea1eb9f2d63a6
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-17 18:52:14 +0200

    Add files via upload

---
+++ 16 - Arrays/Proverb/Proverb/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        var proverbs = new string[subjects.Length];
+        
+        if (proverbs.Length == 0)
+            return proverbs;
+        
+        proverbs[subjects.Length - 1] = "And all for the want of a " + subjects[0] + ".";
+        
+        if (proverbs.Length == 1)
+            return proverbs;
+
+        for (int i = 0; i < subjects.Length - 1; i++)
+        {
+            proverbs[i] = $"For want of a {subjects[i]} the {subjects[i + 1]} was lost.";
+        }
+        
+        return proverbs;
+    }
+}
