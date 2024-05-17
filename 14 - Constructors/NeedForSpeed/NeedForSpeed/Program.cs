c1ebd920f2206aa2ce1b156e5683d978893631c3
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-17 18:10:08 +0200

    Add files via upload

---
+++ 14 - Constructors/NeedForSpeed/NeedForSpeed/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class RemoteControlCar
+{
+    public int Speed { get; }
+    public int BatteryDrain { get; }
+    private int battery;
+    private int distance;
+
+    public RemoteControlCar(int speed, int batteryDrain)
+    {
+        battery = 100;
+        distance = 0;
+        Speed = speed;
+        BatteryDrain = batteryDrain;
+    }
+    public bool BatteryDrained() =>
+        (battery - BatteryDrain) < 0;
+
+    public int DistanceDriven() =>
+        distance;
+    
+    public void Drive()
+    {
+        if (!BatteryDrained())
+        {
+            distance += Speed;
+            battery -= BatteryDrain;
+        }
+    }
+
+    public static RemoteControlCar Nitro() =>
+        new RemoteControlCar(50, 4);
+}
+
+class RaceTrack
+{
+    private readonly int distance;
+
+    public RaceTrack(int distance) => 
+        this.distance = distance;
+    
+    public bool TryFinishTrack(RemoteControlCar car)
+    {
+        var drives = 100 / car.BatteryDrain;
+        var possibleDistance = drives * car.Speed;
+        
+        return possibleDistance >= distance;
+    }
+}
