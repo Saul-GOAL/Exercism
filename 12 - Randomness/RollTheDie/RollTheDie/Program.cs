94cf448cb4a47ca451cad451d5d13fdd65f86d6c
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-17 17:49:54 +0200

    Add files via upload

---
+++ 12 - Randomness/RollTheDie/RollTheDie/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public class Player
+{
+    Random getrandom = new Random();
+
+    public int RollDie() =>
+        getrandom.Next(1, 19);
+    public double GenerateSpellStrength() =>
+        getrandom.NextDouble() * 100;
+}
