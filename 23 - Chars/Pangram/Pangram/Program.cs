68ce5d4517187d1bc615d8c36cc5a2fc0c1ec5a5
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-23 17:49:15 +0200

    Add files via upload

---
+++ 23 - Chars/Pangram/Pangram/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Pangram
+{
+    public const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    public static void Main()
+    { 
+        var phrase = "The quick brown fox jumps over the lazy dog.";
+
+        bool isPangram = IsPangram(phrase);
+
+    }
+
+    public static bool IsPangram(string phrase) => alphabet.All(phrase.ToLower().Contains);
+
+}
