e5f84d28709a607ba7235b1ebf5dc5423005ccac
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-23 17:56:49 +0200

    Add files via upload

---
+++ 24 - String Builder/SqueakyClean/SqueakyClean/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+public static class Identifier
+{
+    public static string Clean(string identifier)
+    {
+        if (!string.IsNullOrWhiteSpace(identifier))
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < identifier.Length; i++)
+            {
+                if (char.IsWhiteSpace(identifier[i]))
+                {
+                    sb.Append('_');
+                    continue;
+                }
+                else if (char.IsControl(identifier[i]))
+                {
+                    sb.Append("CTRL");
+                    continue;
+                }
+                else if (identifier[i] == '-')
+                {
+                    if (i + 1 != identifier.Length)
+                    {
+                        ++i;
+                        sb.Append(char.ToUpper(identifier[i]));
+                    }
+                    continue;
+                }
+                else if (identifier[i] is >= 'α' and <= 'ω')
+                {
+                    continue;
+                }
+                if (char.IsLetter(identifier[i]))
+                {
+                    sb.Append(identifier[i]);
+                }
+            }
+            return sb.ToString();
+        }
+
+        return string.Empty;
+    }
+}
