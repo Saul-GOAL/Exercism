116f3a549153fadf50b549869be7ba1af93bc5ce
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-23 17:48:51 +0200

    Add files via upload

---
+++ 23 - Chars/Isogram/Isogram/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Isogram
+{
+    public static void Main(string[] args)
+    {
+        var word = "lumberjacks";
+
+        var isIsogram = IsIsogram(word);
+
+        Console.WriteLine(isIsogram);
+        Console.ReadLine();
+    }
+
+    public static bool IsIsogram(string word)
+    {
+        var lowerLetters = word.ToLower().Where(char.IsLetter).ToList();
+
+        return lowerLetters.Distinct().Count() == lowerLetters.Count();
+    }
+}
