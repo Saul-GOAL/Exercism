405693d5f58b6e42c6d6b325eb2d059c44ae8bb2
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-23 18:28:57 +0200

    Add files via upload

---
+++ 27 - Enums/LogsLogsLogs/LogsLogsLogs/LogLevel.cs
@@ -0,0 +1,10 @@
+﻿internal enum LogLevel
+{
+    Unknown = 0,
+    Trace = 1,
+    Debug = 2,
+    Info = 4,
+    Warning = 5,
+    Error = 6,
+    Fatal = 42,
+}
---
+++ 27 - Enums/LogsLogsLogs/LogsLogsLogs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+internal enum LogLevel
+{
+    Unknown = 0,
+    Trace = 1,
+    Debug = 2,
+    Info = 4,
+    Warning = 5,
+    Error = 6,
+    Fatal = 42,
+}
+
+static class LogLine
+{
+    private static readonly string pattern = @"\[(?<logLevel>[A-Z]+)\]";
+
+    public static LogLevel ParseLogLevel(string logLine) =>
+        Regex.Match(logLine, pattern).Groups["logLevel"].Value
+        switch
+        {
+            "TRC" => LogLevel.Trace,
+            "DBG" => LogLevel.Debug,
+            "INF" => LogLevel.Info,
+            "WRN" => LogLevel.Warning,
+            "ERR" => LogLevel.Error,
+            "FTL" => LogLevel.Fatal,
+            _ => LogLevel.Unknown,
+        };
+
+    public static string OutputForShortLog(LogLevel logLevel, string message) =>
+        $"{logLevel.GetHashCode()}:{message}";
+}
