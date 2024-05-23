600f256968dfefe82f636536824382d1a73574aa
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-23 19:31:26 +0200

    Add files via upload

---
+++ 12 - Randomness/RobotName/RobotName/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+public class Robot
+{
+    static HashSet<string> robotName = new();
+    Random rand = new();
+    public string Name
+    {
+        get;
+        private set;
+    }
+    
+    public Robot() => 
+        Reset();
+    
+    public void Reset()
+    {
+        do
+        {
+            //rand.Next(65, 91) gets a Random UpperCase Letter from A to Z
+            Name = $"{(char)rand.Next(65, 91)}{(char)rand.Next(65, 91)}{rand.Next(0, 10)}{rand.Next(0, 10)}{rand.Next(0, 10)}";
+        } while (robotName.Contains(Name));
+        
+        robotName.Add(Name);
+    }
+
+}
