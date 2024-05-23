4be0e8921138caf650af6ff269e2ca7c385167b6
Sauru <103905339+Saul-GOAL@users.noreply.github.com>
2024-05-23 17:48:20 +0200

    Add files via upload

---
+++ 23 - Chars/Acronym/Acronym/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+public class Acronym
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Please, write your phrase here: ");
+        var phrase = Console.ReadLine();
+
+        var reponse = GetTheAcronym(phrase);
+
+        Console.WriteLine(reponse);
+
+        Console.ReadLine();
+
+    }
+
+    public static string GetTheAcronym(string phrase)
+    {
+        char[] separators = { ' ', '_', '-' };
+        StringBuilder sb = new();
+
+        var words = phrase.Split(separators);
+
+        foreach (var word in words)
+        {
+            if (!String.IsNullOrEmpty(word))
+                sb.Append(word.Substring(0,1).ToUpper());
+        }
+
+        return sb.ToString();
+    }
+}
